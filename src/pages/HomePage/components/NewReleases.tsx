6577f1b2225c3423c63d570f62f57791a8240f87
Yunho <yunho@Yunhoui-MacBookAir.local>
2025-06-04 23:32:02 +0900

    리액트 쿼리

---
+++ src/pages/HomePage/components/NewReleases.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const NewReleases = () => {
+  return (
+    <div>
+      <Typography
+        variant={"h1"}
+        display={"flex"}
+        justifyContent={"flex-start"}
+        alignItems={"center"}
+      >
+        NewReleases Albums
+      </Typography>
+    </div>
+  );
+};
+
+export default NewReleases;
