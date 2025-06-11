ff86789d3bb628d7606768f64ff3e78973fe67bf
Yunho <yunho@Yunhoui-MacBookAir.local>
2025-06-12 02:22:38 +0900

    css 수정

--- src/pages/PlaylistPage/PlaylistDetailPage/components/DesktopPlaylistItem.tsx
+++ src/pages/PlaylistPage/PlaylistDetailPage/components/DesktopPlaylistItem.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { TPlaylistTrack } from "../../../../model/playlist";
 import { Box, TableCell, TableRow } from "@mui/material";
 import { TEpisode, TTrack } from "../../../../model/track";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import dayjs from "dayjs";
 
 const DesktopPlaylistItem = ({
   item,
@@ -13,10 +16,24 @@ const DesktopPlaylistItem = ({
   const isEpisode = (track: TTrack | TEpisode): track is TEpisode => {
     return "description" in track;
   };
+
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  const formatDuration = (ms: number | undefined) => {
+    if (typeof ms === "number") {
+      const totalSeconds = Math.floor(ms / 1000);
+      const minutes = Math.floor(totalSeconds / 60);
+      const seconds = totalSeconds % 60;
+      return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    }
+    return false;
+  };
+
   return (
     <TableRow
       sx={{
-        borderBottom: "none",
+        "& td": { border: 0 },
+        color: "red",
         "&:hover": {
           backgroundColor: "rgba(255, 255, 255, 0.08)",
           cursor: "pointer",
@@ -28,8 +45,15 @@ const DesktopPlaylistItem = ({
       <TableCell>
         {isEpisode(item.track) ? "N/A" : item.track.album?.name}
       </TableCell>
-      <TableCell>{item.added_at || "알수 없음"}</TableCell>
-      <TableCell>{item.track.duration_ms || "알수 없음"}</TableCell>
+      <TableCell>
+        {dayjs
+          .utc(item.added_at)
+          .tz("Asia/Seoul")
+          .format("YYYY년 MM월 DD일 HH:mm") || "알수 없음"}
+      </TableCell>
+      <TableCell>
+        {formatDuration(item.track.duration_ms) || "알수 없음"}
+      </TableCell>
     </TableRow>
   );
 };
